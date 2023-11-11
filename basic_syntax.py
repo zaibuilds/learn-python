ef185b08aef619e6eb7dfc1567fb621881c9d989
Zai Builds <hello@zaibuilds.io>
2023-11-11 22:54:59 +0000

    study

--- basic_syntax.py
+++ basic_syntax.py
@@ -2,4 +2,29 @@
 
 x = 5;
 
-print(x)
+# print(x)
+
+# Create a variable name and set it to your name as a string.
+
+name = 'Zaina'
+
+# print(name)
+
+# Create a variable age and set it to your age as an integer.
+
+age = 23;
+
+# print(age)
+
+# Create a variable height and set it to your height as a float.
+
+height = 160.02;
+
+# print(height)
+
+# Calculate the sum of two numbers, a and b.
+
+a = 4
+b = 6
+
+print(a+b)
